a86ef07bb79bb889d1262360aab4aba3b78baa0b
kimjg <jgkim4124@gmail.com>
2020-02-16 16:43:45 +0900

    Initial Commit

---
+++ Bolierplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bolierplate
+//
+//  Created by JiGwang Kim on 2020/02/16.
+//  Copyright © 2020 김지광. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
